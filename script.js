1fee1e657ce44c9776177cd36d7ae1e50413b21c
Leandro Batista <leandrobcrv@gmail.com>
2022-09-01 05:49:19 -0300

    Alteraçoes nos requisitos 1 ao 4

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// ===================================================================================
+// Variáveis globais
+
+const paleta = document.querySelectorAll('.color');
+const botao = document.getElementById('button-random-color');
+const elementosPaleta = document.querySelectorAll('#cor1, #cor2, #cor3, #cor4');
+
+// ===================================================================================
+// Função para gerar cores aleatórias
 function gerarCores () {
     const caracteres = '0123456789ABCDEF'
     let color = '#';
@@ -5,25 +14,36 @@ function gerarCores () {
     for(let index=0; index<6; index+=1){
         color+= caracteres.charAt(Math.floor(Math.random() * caracteres.length))
     }
-    
     return color;
 }
 
-const cor1 = document.querySelector('#cor1');
-cor1.style.backgroundColor = 'black';
-
-window.onload = function () {
-
-    const botao = document.getElementById('button-random-color');
+// ===================================================================================
+// Função para criar a paleta de cores
+function criarPaleta () {
 
     botao.addEventListener('click', function(){
-
-        const cor2 = document.querySelector('#cor2');
-        const cor3 = document.querySelector('#cor3');
-        const cor4 = document.querySelector('#cor4'); 
-
-        cor2.style.backgroundColor = gerarCores();
-        cor3.style.backgroundColor = gerarCores();
-        cor4.style.backgroundColor = gerarCores();
+        for(let index = 0; index<=paleta.length-1; index+=1){
+            if(index === 0){
+                paleta[index].style.backgroundColor = 'black';
+            } else {
+                paleta[index].style.backgroundColor = gerarCores();
+            }
+        }
     });
 }
+
+criarPaleta();
+
+// ===================================================================================
+// Função para selecionar a cor
+function selecionarCor(clicado){
+    const corSelecionada = document.querySelector('.selected')
+    corSelecionada.classList.remove('selected');
+    clicado.target.classList.add('selected');
+}
+
+    for(let key of elementosPaleta){
+        key.addEventListener('click', selecionarCor);
+    }
+
+// ===================================================================================
